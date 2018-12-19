d464261a03a591e700357e0beb0a0a7af7293a7b
hiteh <hubert.warzycha@gmail.com>
2018-12-19 23:28:05 +0100

    Add changes to register form

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -107,56 +107,35 @@
                 <div class="subtitle m-b-md">
                     Hi, it seems that there is no users registered. Go ahead and create root user!
                 </div>
-                <div class="form-body">
+                <div>
                     <form method="POST" action="{{ route('register') }}">
                         @csrf
 
                         <div>
-                            <label for="name" class="col-md-4 col-form-label text-md-right">{{ __('Name') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="name" type="text" class="form-control{{ $errors->has('name') ? ' is-invalid' : '' }}" name="name" value="{{ old('name') }}" required autofocus>
-
-                                @if ($errors->has('name'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('name') }}</strong>
-                                    </span>
-                                @endif
+                            <label for="name">{{ __('Name') }}</label>
+                            <div>
+                                <input id="name" type="text" name="name" value="" required autofocus>
                             </div>
                         </div>
 
                         <div>
-                            <label for="email" class="col-md-4 col-form-label text-md-right">{{ __('E-Mail Address') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="email" type="email" class="form-control{{ $errors->has('email') ? ' is-invalid' : '' }}" name="email" value="{{ old('email') }}" required>
-
-                                @if ($errors->has('email'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('email') }}</strong>
-                                    </span>
-                                @endif
+                            <label for="email">{{ __('E-Mail Address') }}</label>
+                            <div>
+                                <input id="email" type="email" name="email" value="" required>
                             </div>
                         </div>
 
                         <div>
-                            <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
-
-                            <div class="col-md-6">
-                                <input id="password" type="password" class="form-control{{ $errors->has('password') ? ' is-invalid' : '' }}" name="password" required>
-
-                                @if ($errors->has('password'))
-                                    <span class="invalid-feedback" role="alert">
-                                        <strong>{{ $errors->first('password') }}</strong>
-                                    </span>
-                                @endif
+                            <label for="password">{{ __('Password') }}</label>
+                            <div>
+                                <input id="password" type="password" name="password" value="" required>
                             </div>
                         </div>
 
                         <div>
                             <label for="password-confirm">{{ __('Confirm Password') }}</label>
                             <div>
-                                <input id="password-confirm" type="password" name="password_confirmation" required>
+                                <input id="password-confirm" type="password" name="password_confirmation" value="" required>
                             </div>
                         </div>
 
