550cb50041915338d872dfa67bfb8c99e328c0c7
hiteh <hubert.warzycha@gmail.com>
2018-12-20 00:33:35 +0100

    Add Role model with controller and migrations table

--- app/User.php
+++ app/User.php
@@ -27,4 +27,13 @@ class User extends Authenticatable
     protected $hidden = [
         'password', 'remember_token',
     ];
+
+    /**
+     *
+     * Relationship with roles
+     *
+     */
+    public function roles() {
+        return $this->belongToMany(Role::class);
+    }
 }
