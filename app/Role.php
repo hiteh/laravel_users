c63be644a6177d05ecae6767119abbb855e2304f
hiteh <hubert.warzycha@gmail.com>
2019-01-03 22:43:06 +0100

    Fix methods names

--- app/Role.php
+++ app/Role.php
@@ -22,6 +22,6 @@ class Role extends Model
      *
      */
     public function users() {
-        return $this->belongToMany(User::class);
+        return $this->belongsToMany(User::class);
     }
 }
--- app/User.php
+++ app/User.php
@@ -34,6 +34,6 @@ class User extends Authenticatable
      *
      */
     public function roles() {
-        return $this->belongToMany(Role::class);
+        return $this->belongsToMany(Role::class);
     }
 }
