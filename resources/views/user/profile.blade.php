daf0ec9cb89825bc18c4097a86c31734f5ebaf25
hiteh <hubert.warzycha@gmail.com>
2019-01-25 17:09:04 +0100

    Update profile view

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -8,20 +8,28 @@
                     <div class="card">
                         <div class="card-header">{{ __('profile.profile') }}</div>
                         <div class="card-body">
-                            <ul class="list-group">
-                                <li class="list-group-item d-flex p-0">
-                                    <div class="list-group-item-label p-2">{{ __( 'profile.first_name' ) }}</div>
-                                    <div class="list-group-item-value p-2">{{ $user->name }}</div>
-                                </li>
-                                <li class="list-group-item d-flex p-0">
-                                    <div class="list-group-item-label p-2">{{ __( 'profile.mail' ) }}</div>
-                                    <div class="list-group-item-value p-2">{{ $user->email }}</div>
-                                </li>
-                                <li class="list-group-item d-flex p-0">
-                                    <div class="list-group-item-label p-2">{{ __( 'profile.role' ) }}</div>
-                                    <div class="list-group-item-value p-2">{{ $user->roles()->first()->name }}</div>
-                                </li>
-                            </ul>
+                            <div class="row">
+                                <div class="col-9">
+                                    <ul class="list-group">
+                                        <li class="list-group-item d-flex p-0">
+                                            <div class="list-group-item-label p-2">{{ __( 'profile.first_name' ) }}</div>
+                                            <div class="list-group-item-value p-2">{{ $user->name }}</div>
+                                        </li>
+                                        <li class="list-group-item d-flex p-0">
+                                            <div class="list-group-item-label p-2">{{ __( 'profile.mail' ) }}</div>
+                                            <div class="list-group-item-value p-2">{{ $user->email }}</div>
+                                        </li>
+                                        <li class="list-group-item d-flex p-0">
+                                            <div class="list-group-item-label p-2">{{ __( 'profile.role' ) }}</div>
+                                            <div class="list-group-item-value p-2">{{ $user->roles()->first()->name }}</div>
+                                        </li>
+                                    </ul>
+                                </div>
+                                <div class="col-3 d-flex justify-content-center align-items-center">
+                                    <i class="fas fa-user fa-5x"></i>
+                                </div>
+                            </div>
+
                         </div>
                     </div>
                     <div class="d-flex justify-content-end">
@@ -31,6 +39,10 @@
                         </p>
                         <p data-placement="top" data-toggle="tooltip" title="{{ __( 'profile.edit_password' ) }}" class="m-0" >
                             <button class="btn btn-primary m-2 btn-new" data-toggle="modal" data-target="#password-modal">      {{ __( 'profile.edit_password' ) }}
+                            </button>
+                        </p>
+                        <p data-placement="top" data-toggle="tooltip" title="{{ __( 'profile.edit_avatar' ) }}" class="m-0" >
+                            <button class="btn btn-primary m-2 btn-new" data-toggle="modal" data-target="#avatar-modal">      {{ __( 'profile.edit_avatar' ) }}
                             </button>
                         </p>
                     </div>
