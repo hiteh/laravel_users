fc32cb9212213cf1bd3f7fbe5fd46cda5057963b
hiteh <hubert.warzycha@gmail.com>
2019-02-12 13:51:44 +0100

    Update profile view

--- resources/views/user/profile.blade.php
+++ resources/views/user/profile.blade.php
@@ -8,32 +8,41 @@
                     <div class="card">
                         <div class="card-header">{{ __('profile.profile') }}</div>
                         <div class="card-body">
-                            <div class="row">
-                                <div class="col-9">
-                                    <ul class="list-group">
-                                        <li class="list-group-item d-flex p-0">
-                                            <div class="list-group-item-label p-2">{{ __( 'profile.first_name' ) }}</div>
-                                            <div class="list-group-item-value p-2">{{ $user->name }}</div>
-                                        </li>
-                                        <li class="list-group-item d-flex p-0">
-                                            <div class="list-group-item-label p-2">{{ __( 'profile.mail' ) }}</div>
-                                            <div class="list-group-item-value p-2">{{ $user->email }}</div>
-                                        </li>
-                                        <li class="list-group-item d-flex p-0">
-                                            <div class="list-group-item-label p-2">{{ __( 'profile.role' ) }}</div>
-                                            <div class="list-group-item-value p-2">{{ $user->roles()->first()->name }}</div>
-                                        </li>
-                                    </ul>
+                            <div class="row flex-column-reverse flex-sm-row">
+                                <div class="col-12 col-sm-9">
+                                    <table class="table table-hover">
+                                      <thead>
+                                      </thead>
+                                      <tbody>
+                                        <tr>
+                                          <th scope="row">{{ __( 'profile.first_name' ) }}</th>
+                                          <td>{{ $user->name }}</td>
+                                        </tr>
+                                        <tr>
+                                          <th scope="row">{{ __( 'profile.mail' ) }}</th>
+                                          <td>{{ $user->email }}</td>
+                                        </tr>
+                                        <tr>
+                                          <th scope="row">{{ __( 'profile.role' ) }}</th>
+                                          <td colspan="2">{{ $user->roles()->first()->name }}</td>
+                                        </tr>
+                                      </tbody>
+                                    </table>
                                 </div>
-                                <div class="col-3 d-flex justify-content-center align-items-center">
-                                    <i class="fas fa-user fa-5x"></i>
-                                    <img src="{{ asset( 'storage/'.$user->avatar ) }}" ></img>
+                                <div class="col-12 col-sm-3 d-flex justify-content-center align-items-center">
+                                    <div class="m-1">
+                                        @if ( $user->avatar )
+                                            <img src="{{ asset( 'storage/'.$user->avatar ) }}" class="img-thumbnail p-2"></img>
+                                        @else
+                                            <i class="fas fa-user fa-5x"></i>
+                                        @endif
+                                    </div>
                                 </div>
                             </div>
 
                         </div>
                     </div>
-                    <div class="d-flex justify-content-end">
+                    <div class="d-flex justify-content-center justify-content-sm-end flex-wrap">
                         <p data-placement="top" data-toggle="tooltip" title="{{ __( 'profile.edit_profile' ) }}" class="m-0" >
                             <button class="btn btn-primary m-2 btn-new" data-toggle="modal" data-target="#profile-modal">      {{ __( 'profile.edit_profile' ) }}
                             </button>
