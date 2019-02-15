5e7f2309bc03e98819631eaef14d9250c8462943
hiteh <hubert.warzycha@gmail.com>
2019-02-15 20:34:48 +0100

    Fix users repository interface

--- app/Interfaces/UsersRepositoryInterface.php
+++ app/Interfaces/UsersRepositoryInterface.php
@@ -7,11 +7,11 @@ interface UsersRepositoryInterface
 {
 	public function getAllUsers( array $order, int $items_per_page );
 
-	public function getUserById( $id );
+	public function getUserById( string $id );
 
 	public function addUser( array $data );
 
-	public function deleteUser( $id );
+	public function deleteUser( string $id );
 
-	public function updateUser( $id, array $data );
+	public function updateUser( string $id, array $data );
 }
