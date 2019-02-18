3370263b6de66c2eff78617d668583eccbb0f30e
hiteh <hubert.warzycha@gmail.com>
2019-02-18 10:14:56 +0100

    Update profile controller

--- app/Http/Controllers/ProfileController.php
+++ app/Http/Controllers/ProfileController.php
@@ -4,11 +4,9 @@ namespace App\Http\Controllers;
 
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
+use App\Interfaces\UserDataValidationInterface;
+use App\Interfaces\UsersRepositoryInterface;
 use Illuminate\Support\Facades\Auth;
-use Illuminate\Support\Facades\Hash;
-use Illuminate\Support\Facades\Validator;
-use Illuminate\Support\Facades\Storage;
-
 
 class ProfileController extends Controller
 {
@@ -17,10 +15,12 @@ class ProfileController extends Controller
      *
      * @return void
      */
-    public function __construct()
+    public function __construct( UserDataValidationInterface $validator, UsersRepositoryInterface $users )
     {
     	$this->middleware( 'auth' );
         $this->middleware( 'profile' );
+        $this->validator = $validator;
+        $this->users = $users;
     }
     
     /**
@@ -37,67 +37,14 @@ class ProfileController extends Controller
     /**
      * Update user's profile.
      *
-     * @param string $id
      * @param \Illuminate\Http\Request $request
      * @return \Illuminate\Http\Response
      */
-    public function update( $id, Request $request )
+    public function update( Request $request )
     {
-        $data = $this->validator( $request->all(), $id )->validate();
-        $user = Auth::user();
+        $data = $this->validator->validateUserData( $request->all(), Auth::user()->id );
+        $user = $this->users->updateUser( Auth::user()->id, $data );
 
-        if ( $user )
-        {
-            foreach ( $data as $field => $value )
-            {
-                if ( 'password' === $field )
-                {
-                    $user->update([
-                        $field => Hash::make( $value ),
-                    ]);
-                }
-                else if ( 'avatar' === $field )
-                {
-                    $value->store('public');
-
-                    Storage::delete( 'public/'.$user->avatar );
-
-                    $user->update([
-                        'avatar' => $value->hashName(),
-                    ]);
-                }
-                else
-                {
-                    $user->update([
-                        $field => $value,
-                    ]);
-                }
-
-            }
-
-            return redirect()->route( 'profile', $id )->with( ['success' => __('profile.update_success_msg') ] );
-        }
-        else
-        {
-            return redirect()->route( 'profile', $id )->with( ['warning' => __('profile.invalid_user_msg') ] );
-        }
-
-    }
-
-    /**
-     * Request validator.
-     *
-     * @param  array $data
-     * @param  string $id
-     * @return \Illuminate\Contracts\Validation\Validator
-     */
-    protected function validator( array $data, $id = null )
-    {
-        return Validator::make( $data, [
-            'name'     => ['sometimes','required', 'string', 'max:255'],
-            'email'    => ['sometimes','required', 'string', 'email', 'max:255', 'unique:users,email,'.$id ],
-            'password' => ['sometimes','required', 'string', 'min:6', 'confirmed'],
-            'avatar'   => ['sometimes', 'required', 'image'],
-        ] );
+        return redirect()->route( 'profile' )->with( ['success' => __('profile.update_success_msg') ] );
     }
 }
