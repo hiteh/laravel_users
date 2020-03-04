08fcb90f398df86adbf97fa7c92f6f45109479fc
hiteh <hubert.warzycha@gmail.com>
2020-03-05 00:35:31 +0100

    Add missing translation.

---
+++ resources/lang/en/home.php
@@ -0,0 +1,5 @@
+<?php
+
+return [
+    'user_msg' => 'You are logged in!',
+];
--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,8 +13,7 @@
                             {{ session('status') }}
                         </div>
                     @endif
-
-                    You are logged in!
+                    <p>{{ __('home.user_msg') }}</p>
                 </div>
             </div>
         </div>
